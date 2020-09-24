f8ed155b029ef6fad6829675d574eefc9b021d85
jackywang <jacky.android@foxmail.com>
2020-09-24 22:12:43 +0800

    update content.

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,15 @@ set -e
 cloudbase framework:deploy
 
 # 发布生成的文件
-cd docs
+# cd docs
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
-git push -f git@github.com:JackyAndroid/AndroidInterview-Q-A.git master:gh-pages
+# git push -f git@github.com:JackyAndroid/AndroidInterview-Q-A.git master:gh-pages
 
-cd -
+# cd -
 
 # 发布主分支的内容
 git add -A
